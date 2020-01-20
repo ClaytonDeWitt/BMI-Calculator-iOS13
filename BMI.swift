9189b2a26772da0e8e66718e4015759e1ddacf3d
Clayton DeWitt <cdnp44@gmail.com>
2020-01-19 18:24:48 -0800

    First commit

---
+++ BMI.swift
@@ -0,0 +1,17 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Clay on 11/7/19.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+
+struct BMI {
+    
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
